2877653fcaa7492c873952b6b9bf3b687fce5f4c
mariok99 <marioaleliv@hotmail.com>
2023-09-08 20:14:26 -0300

    corrección para devolver posiciones en lugar de valores

--- ej3.py
+++ ej3.py
@@ -5,24 +5,29 @@
 #algun partido pasa el 3% y siempre hay forma de que se ocupen todos los cargos
 def obtenerSenadoresEnProvincia(escrutinio:list[int]):
     id:int = 0
-    first: int = 0
-    second: int = 0
+    first: int = [0,0] #en la posición cero va el valor, en la 1 la posición
+    second: int = [0,0]
     while id < len(escrutinio)-1:
-        if first < escrutinio[id]:
-            second = first
-            first = escrutinio[id]    
+        if first[0] < escrutinio[id]:
+            second[0] = first[0]
+            second[1] = first[1]
+            first[0] = escrutinio[id]
+            first[1] = id  
+        else:
+            pass
         
-        if second < escrutinio[id] and escrutinio[id] < first:
-            second = escrutinio[id]
+        if second[0] < escrutinio[id] and escrutinio[id] < first[0]:
+            second[0] = escrutinio[id]
+            second[1] = id
             
         else:
             pass
         id = id + 1
-    res = (first,second)
+    res = (first[1],second[1])
     return res
 
 #pruebas 
-#[123,44523,43,23456734] BIEN
-#[10,9,1,1000] BIEN
-#[10,11,9,12,0] BIEN
-#[1,17,100,120,105,10000,1000000000000]
+#[123,44523,43,23456734] == (1,0) BIEN
+#[10,9,1,1000]  == (0,1) BIEN
+#[10,11,9,12,0] == (3,1) BIEN
+#[1,17,100,120,105,10000,1000000000000] == (5,3)
